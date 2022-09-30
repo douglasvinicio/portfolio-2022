24a0165914fe2c1ff79af2fd207066844ffbcece
douglasvinicio <douglas.vinicio@hotmail.com>
2022-09-29 22:28:47 -0400

    About started / Added items and quote

--- frontend-react/src/constants/images.js
+++ frontend-react/src/constants/images.js
@@ -35,6 +35,7 @@ import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
+import arrow from '../assets/arrow.gif';
 
 export default {
   email,
@@ -71,4 +72,5 @@ export default {
   nb,
   skype,
   spotify,
+  arrow
 };
--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -1,7 +1,47 @@
+import React, { useState, useEffect } from 'react'
+import { motion } from 'framer-motion'
+
+import { images } from '../../constants'
 import './About.scss'
+
+const abouts = [
+    {title: 'Web Development', description: 'I am a good web developer', imgURL: images.about01},
+    {title: 'Web Development', description: 'I am a good web developer', imgURL: images.about02},
+    {title: 'Web Development', description: 'I am a good web developer', imgURL: images.about03},
+    {title: 'Web Development', description: 'I am a good web developer', imgURL: images.about04},
+]
+
+const container = {
+    visible: {
+      transition: {
+        staggerChildren: 0.025
+      }
+    }
+  };
 
 export function About() {
     return (
-        <h1>About</h1>
+        <>
+          {/* <img src={images.arrow} alt="arrow-poiting-down" className="arrow-pointing-quote"/>   */}
+            <h2 className="about-quote">               
+            “If you wish to <span>improve</span>, be content to <span>be thought <br /> foolish and stupid</span>” <span> - Epictetus - </span> 
+            </h2>
+
+            <div className="app__profiles">
+                {abouts.map((about, index) => (
+                    <motion.div
+                        whileInView = {{ opacity: 1}}
+                        whileHover = {{ scale: 1.1}}
+                        treansition = {{ duration: 0.5, type: 'tween'}}
+                        className="app__profile-item"
+                        key={about.title + index}
+                    >
+                        <img src={about.imgURL} alt={about.title} />
+                        <h2 className="bold-text" style={{ marginTop: 20}}> { about.title }</h2>
+                        <p className="p-text" style={{ marginTop: 10}}>{ about.description }</p>
+                    </motion.div>
+                ))}
+            </div>
+        </>
     )
 }
